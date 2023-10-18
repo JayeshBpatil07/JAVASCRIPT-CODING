14f506f71527723fd4360363f76f70bd08268a83
JayeshBpatil07 <jayeshpatil150401@gmail.com>
2023-10-18 16:21:29 +0530

    datatypes basics

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,14 @@
+// In the javascript there is inbuilt fuctionality of some datatypes which is already defined
+
+name = 'jayesh' //string
+age = 22 //number
+BigInt = 1.02562553 //uses in stock market or index websites
+Boolean =  true // yes or no
+null // object
+undefined //undefined => const myname;
+Symbol //uniqueness uses only
+
+
+//objects declartion in output
+
+console.table(typeof undefined);
