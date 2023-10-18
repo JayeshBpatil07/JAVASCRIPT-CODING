9f3d44cfbc0449a358b958af5b1db858ee6393e1
JayeshBpatil07 <jayeshpatil150401@gmail.com>
2023-10-18 15:57:50 +0530

    variables  basics

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,25 @@
+const myName = "jayesh patil"; //not changable
+
+let my_age = 22;  //changeble
+
+var Mytown = 'Amalner' //prefer not to use in today's condition
+
+mycollege = 'RCPIT'  //js accecepts this type of variables
+
+let yourname;  //show undefined variable
+
+
+// we can chage let and var variables but not const
+my_age = 25
+Mytown = 'bhilali'
+
+
+// types of getting output in terminal
+
+const myoutput = console.log(myName, my_age, Mytown, mycollege, yourname);
+
+console.table([myName, my_age, Mytown, mycollege, yourname])
+
+
+
+
