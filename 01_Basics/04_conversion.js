69be98caf3a7d9b3ebd6e36358eeba9ac5760641
JayeshBpatil07 <jayeshpatil150401@gmail.com>
2023-10-19 10:20:40 +0530

    converion js basics

---
+++ 01_Basics/04_conversion.js
@@ -0,0 +1,15 @@
+console.log(4>=5);
+
+console.log(null == 5);
+console.log(null == 0);
+console.log(null <=0);
+console.log(null>=0);
+console.log(null<0);
+
+
+console.log(undefined ==0);
+console.log(undefined>=1);
+console.log(undefined<=0);
+console.log(undefined===0);
+
+console.log("5" ===4);
