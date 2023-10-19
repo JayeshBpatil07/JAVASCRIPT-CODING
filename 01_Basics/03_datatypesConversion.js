4cb28d16bb13c546dad89dd8a58367fd2ff89068
JayeshBpatil07 <jayeshpatil150401@gmail.com>
2023-10-19 09:58:21 +0530

    update

--- 01_Basics/03_datatypesConversion.js
+++ 01_Basics/03_datatypesConversion.js
@@ -29,3 +29,53 @@ console.log(typeof yourvillage);
 
 convertToBoolean = Boolean(yourvillage)
 console.log(convertToBoolean);
+
+
+
+
+
+
+
+
+// oprations
+
+let marks = 5
+ const outptmarks = -marks
+console.log(outptmarks);
+
+
+let a= 5
+let b= 5
+
+let c= a+b
+let d = a-b
+let e = a*b
+let f = a%b
+let g = a/b
+
+console.log(c, d, e, f, g);
+
+console.log(2+5*5-4);
+
+
+
+
+
+
+// prefix opration
+
+
+let x = 5
+let y = ++x
+// expected output is x=6 and y 6
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+console.log(`x is:${x} and y is:${y}`);
+
+
+// postfix operation
+
+let m = 7
+let n = m++
+//expected output is m=8 and n=7
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+console.log(`m is:${m} and n is:${n}`);
